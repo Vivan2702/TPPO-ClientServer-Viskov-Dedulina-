3ebc53ef83c80417aacb4e4d36ee2be02c7c5a8e
Vivan2702 <100070816+Vivan2702@users.noreply.github.com>
2023-05-16 22:02:30 +0700

    Добавление функционала
    Добавлена передача сообщений сервером от клиента к клиенту

--- Server/src/common.h
+++ Server/src/common.h
@@ -12,18 +12,30 @@
 
 using namespace std;
 
-DWORD WINAPI serverReceive(LPVOID lpParam) {
+struct clients {
+	SOCKET cl1;
+	SOCKET cl2;
+};
 
+DWORD WINAPI serverReceive(void* p) {
+
+	clients * d = ((clients*)p);
 	char login[64] = { 0 };
 	char buffer[1024] = { 0 };
-	SOCKET client = *(SOCKET*)lpParam;
-	if (recv(client, login, sizeof(login), 0) == SOCKET_ERROR) {
+	SOCKET client1 = d->cl1;
+	SOCKET client2 = d->cl2;
+
+	if (recv(client1, login, sizeof(login), 0) == SOCKET_ERROR) {
 		cout << "Ошибка, логин не получен" << WSAGetLastError() << endl;
+		return -1;
+	}
+	if (send(client2, login, sizeof(buffer), 0) == SOCKET_ERROR) {
+		cout << "Ошибка отправки данных " << WSAGetLastError() << endl;
 		return -1;
 	}
 	while (true) {
 
-		if (recv(client, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
+		if (recv(client1, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
 
 			cout << "Ошбика принятия данных" << WSAGetLastError() << endl;
 			return -1;
@@ -32,16 +44,26 @@ DWORD WINAPI serverReceive(LPVOID lpParam) {
 			cout << "Произошло отключение клиента " << login << endl;
 			continue;
 		}
-		cout << "Клиент " << login << " - " << buffer << endl;
+		login[strlen(login) - 1] = ' ';
+		buffer[strlen(buffer) - 1] = ' '; // убирание перевода строки для нормального вывода
+		if (strlen(buffer) != 0)
+		{
+			cout << "Клиент " << login << " - " << buffer << endl;
+		}
+		if (send(client2, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
+			cout << "Ошибка отправки данных " << WSAGetLastError() << endl;
+			return -1;
+		}
 		memset(buffer, 0, sizeof(buffer)); //Очистить буфер
+		
 	}
 	return 1;
 }
 
-DWORD WINAPI serverSend(LPVOID lpParam) {
-
+DWORD WINAPI serverSend(void* p) {
+	clients * d = (clients*)p;
 	char buffer[1024] = { 0 };
-	SOCKET client = *(SOCKET*)lpParam;
+	SOCKET client = d->cl1;
 	while (true) {
 		fgets(buffer, 1024, stdin);
 		if (send(client, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
